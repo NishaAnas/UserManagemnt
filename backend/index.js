43dad3b1e74511b779620894b723e695c2a7f791
nishamashhood <mashhoodnisha@gmail.com>
2024-11-04 21:51:59 +0530

    database connected successfully

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,14 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config();
 const PORT = 3000;
 
-
+mongoose.connect(process.env.MONGODB_CONNECT_URL).then(()=>{
+    console.log('Database Connected successfully')
+}).catch((err)=>{
+    console.log(`Error in Datavase Connection: ${err}`);
+})
 const app = express();
 
 app.listen(PORT, ()=>{
