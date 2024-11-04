2366a5a8b16c563c03052bb73be170dad2387d36
nishamashhood <mashhoodnisha@gmail.com>
2024-11-04 22:28:11 +0530

    created and tested the routes and controllers

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,26 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 dotenv.config();
 const PORT = 3000;
+import UserRoutes from './Routes/user.routes.js'
 
+
+//Connect MongoDb
 mongoose.connect(process.env.MONGODB_CONNECT_URL).then(()=>{
     console.log('MongoDB Connected successfully')
 }).catch((err)=>{
     console.log(`Error in MongoDB Connection: ${err}`);
 })
+
+//Configuring express
 const app = express();
 
+
+
+//Configuring Routes
+app.use('/server/user',UserRoutes);
+
+
+//Listening to port
 app.listen(PORT, ()=>{
     console.log(`Server Listening to port ${PORT}`);
 })
